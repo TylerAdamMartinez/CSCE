f05433abaf301b457b2782b981b08df87ca4d662
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-02-23 08:59:36 -0600

    feat: impl add_leaf(data) func to BinaryTree

--- Homework2/src/main.rs
+++ Homework2/src/main.rs
@@ -1,4 +1,3 @@
-
 enum BinaryTree {
     Leaf {
         data: f64,
@@ -22,7 +21,40 @@ impl BinaryTree {
     }
 }
 
+impl BinaryTree {
+    fn add_leaf(&mut self, new_data: f64) {
+        match self {
+            BinaryTree::Leaf {
+                ref data,
+                ref mut left,
+                ref mut right,
+            } => match data {
+                i if i > &new_data => {
+                    left.add_leaf(new_data);
+                    println!("added {} to the left", new_data);
+                }
+                i if i < &new_data => {
+                    right.add_leaf(new_data);
+                    println!("added {} to the right", new_data);
+                }
+                _ => {}
+            },
+            BinaryTree::Null => {
+                *self = BinaryTree::new_leaf(new_data);
+            }
+        }
+    }
+}
+
 fn main() {
-    let binary_search_tree = BinaryTree::new_tree();
-    println!("Hello, world!");
+    let mut binary_search_tree = BinaryTree::new_tree();
+    binary_search_tree.add_leaf(3.32);
+    binary_search_tree.add_leaf(6.43);
+    binary_search_tree.add_leaf(5.32);
+    binary_search_tree.add_leaf(77.32);
+    binary_search_tree.add_leaf(1.23);
+    binary_search_tree.add_leaf(455.23);
+    binary_search_tree.add_leaf(6.43);
+    binary_search_tree.add_leaf(3213.2);
+    binary_search_tree.add_leaf(0.21231);
 }
