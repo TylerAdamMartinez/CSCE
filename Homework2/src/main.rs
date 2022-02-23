0301cd6a8c7731b0b30d285abb859489377038cb
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-02-23 08:16:48 -0600

    feat: impl basic BinaryTree funcs
    new_tree() -> tree leaf of NULL
    new_leaf(data: f64) -> tree leaf of data and two leafs (left and right)
    of NULL

--- Homework2/src/main.rs
+++ Homework2/src/main.rs
@@ -1,3 +1,28 @@
+
+enum BinaryTree {
+    Leaf {
+        data: f64,
+        left: Box<BinaryTree>,
+        right: Box<BinaryTree>,
+    },
+    Null
+}
+
+impl BinaryTree {
+    fn new_tree() -> Self {
+        BinaryTree::Null
+    }
+
+    fn new_leaf(data: f64) -> Self {
+        BinaryTree::Leaf {
+            data,
+            left: Box::new(BinaryTree::Null),
+            right: Box::new(BinaryTree::Null),
+        }
+    }
+}
+
 fn main() {
+    let binary_search_tree = BinaryTree::new_tree();
     println!("Hello, world!");
 }
