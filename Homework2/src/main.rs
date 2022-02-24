239af796b928a111a629119f99158afe199c9739
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-02-23 20:50:38 -0600

    feat(main.rs): create func to populate the tree

--- Homework2/src/main.rs
+++ Homework2/src/main.rs
@@ -3,12 +3,8 @@ mod binary_tree;
 
 fn main() {
     let mut binary_search_tree = binary_tree::BinaryTree::new();
-    let mut rng = rand::thread_rng();
 
-    for _n in 0..10 {
-        binary_search_tree.insert(rng.gen_range(0.0..1000.0));
-    }
-
+    populate_binary_tree(&mut binary_search_tree, 100);
     binary_search_tree.insert(1001.0);
 
     println!("binary_search_tree is {:#?}", binary_search_tree);
@@ -17,4 +13,12 @@ fn main() {
     println!("Find 1001.0 in the tree\nStatus: {:?}", binary_search_tree.search(&binary_search_tree, 1001.0));
     println!("Find 1055.0 in the tree\nStatus: {:?}", binary_search_tree.search(&binary_search_tree, 1055.0));
 
+}
+
+fn populate_binary_tree(tree: &mut binary_tree::BinaryTree, elements_count: u64) {
+    let mut rng = rand::thread_rng();
+
+    for _n in 1..elements_count {
+        tree.insert(rng.gen_range(0.0..1000.0));
+    }
 }
