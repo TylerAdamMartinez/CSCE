93dd3ffdb1dfe394a4cc77698fe91fd3c44f2a16
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-02-24 06:36:19 -0600

    feat: create calc time func and get stats func
    In addition, refactor code to group like logic

--- Homework2/src/main.rs
+++ Homework2/src/main.rs
@@ -1,24 +1,35 @@
 use rand::Rng;
-use std::time::Instant;
+use std::time::{Instant, Duration};
 mod binary_tree;
 
 fn main() {
-    let mut binary_search_tree = binary_tree::BinaryTree::new();
+    let mut binary_search_tree_100 = binary_tree::BinaryTree::new();
+    let mut binary_search_tree_1k = binary_tree::BinaryTree::new();
+    let mut binary_search_tree_10k = binary_tree::BinaryTree::new();
 
-    populate_tree(&mut binary_search_tree, 10000);
+    let hundred = 100;
+    let one_thousand = 1000;
+    let ten_thousand = 10000;
 
-    let start_timer = Instant::now();
-    binary_search_tree.insert(1001.0);
-    let end_timer = start_timer.elapsed();
+    populate_tree_rand(&mut binary_search_tree_100, hundred);
+    populate_tree_rand(&mut binary_search_tree_1k, one_thousand);
+    populate_tree_rand(&mut binary_search_tree_10k, ten_thousand);
 
-    print_tree_stats(&binary_search_tree);
-    println!("Find 1001.0 in the tree\nStatus: {:?}", binary_search_tree.search(&binary_search_tree, 1001.0));
-    println!("Find 1055.0 in the tree\nStatus: {:?}", binary_search_tree.search(&binary_search_tree, 1055.0));
+    let tree_stats_100 = get_tree_stats(&binary_search_tree_100);
+    let tree_stats_1k = get_tree_stats(&binary_search_tree_1k);
+    let tree_stats_10k = get_tree_stats(&binary_search_tree_10k);
 
-    println!("time duration: {}ms", end_timer.as_secs() * 1000);
+    let time_counts_of_bst_100 = calc_execution_times(&mut binary_search_tree_100);
+    let time_counts_of_bst_1k = calc_execution_times(&mut binary_search_tree_1k);
+    let time_counts_of_bst_10k = calc_execution_times(&mut binary_search_tree_10k);
+
+    println!("insert: {:?}", time_counts_of_bst_100.insert);
+    println!("search: {:?}", time_counts_of_bst_100.search);
+    println!("size: {:?}", time_counts_of_bst_100.size);
+    println!("depth: {:?}", time_counts_of_bst_100.depth);
 }
 
-fn populate_tree(tree: &mut binary_tree::BinaryTree, elements_count: u64) {
+fn populate_tree_rand(tree: &mut binary_tree::BinaryTree, elements_count: u64) {
     let mut rng = rand::thread_rng();
 
     for _n in 1..elements_count {
@@ -26,8 +37,57 @@ fn populate_tree(tree: &mut binary_tree::BinaryTree, elements_count: u64) {
     }
 }
 
-fn print_tree_stats(tree: &binary_tree::BinaryTree) {
-    println!("{:#?}", tree);
-    println!("Number of elements: {}", tree.size(&tree));
-    println!("Max depth of tree: {}", tree.depth(&tree));
+struct BinaryTreeStats {
+    size: u64,
+    depth: u64,
 }
+
+fn get_tree_stats(tree: &binary_tree::BinaryTree) -> BinaryTreeStats {
+    BinaryTreeStats {
+        size: tree.size(&tree),
+        depth: tree.depth(&tree),
+    }
+}
+
+struct TimeCounts {
+    insert: Duration,
+    size: Duration,
+    depth: Duration,
+    search: Duration,
+}
+
+fn calc_execution_times(tree: &mut binary_tree::BinaryTree) -> TimeCounts {
+    // Caculations the time of the size() method
+    let start_timer = Instant::now();
+    let tree_size = tree.size(&tree);
+    let end_timer = Instant::now();
+    let size_time = end_timer.duration_since(start_timer);
+
+    let new_element = tree_size as f64 + 1.0;
+
+    // Caculations the time of the insert() method
+    let start_timer = Instant::now();
+    tree.insert(new_element);
+    let end_timer = Instant::now();
+    let insert_time = end_timer.duration_since(start_timer);
+
+    // Caculations the time of the search() method
+    let start_timer = Instant::now();
+    tree.search(&tree, new_element);
+    let end_timer = Instant::now();
+    let search_time = end_timer.duration_since(start_timer);
+
+    // Caculations the time of the depth() method
+    let start_timer = Instant::now();
+    tree.depth(&tree);
+    let end_timer = Instant::now();
+    let depth_time = end_timer.duration_since(start_timer);
+
+    TimeCounts {
+        size: size_time,
+        insert: insert_time,
+        search: search_time,
+        depth: depth_time,
+    }
+}
+
