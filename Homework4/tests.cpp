7561198f52762c127eb1ae0dd7e8617c61c515d3
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-04-15 20:59:57 -0500

    test: impl Insertation sort test

--- Homework4/tests.cpp
+++ Homework4/tests.cpp
@@ -12,23 +12,68 @@ int main() {
   using namespace std;
   cout << "homework 4 unit tests" << endl;
 
-  myFirstTest();
+  InsertionSortTest();
+  SelectionSortTest();
   return 0;
 }
 
-void myFirstTest() {
+void InsertionSortTest() {
   using namespace std;
 
-  vector<int> myvec;
-  myvec.push_back(45);
-  vector<int> mysecondvec;
-  mysecondvec.push_back(45);
-  assertm(myvec == mysecondvec, "failed here..");
-  cout << "test was successful" << endl;
+  vector<int> control_array;
+  control_array.push_back(2);
+  control_array.push_back(4);
+  control_array.push_back(53);
+  control_array.push_back(65);
+  control_array.push_back(165);
+  control_array.push_back(238);
+  control_array.push_back(432);
+  control_array.push_back(876);
+
+  vector<int> array;
+  array.push_back(4);
+  array.push_back(432);
+  array.push_back(53);
+  array.push_back(238);
+  array.push_back(2);
+  array.push_back(65);
+  array.push_back(165);
+  array.push_back(876);
+
+  InsertionSort(array);
+  assertm(control_array == array, "InsertionSortTest [failed]");
+  cout << "InsertionSortTest [passed]" << endl;
+
 }
 
-void InsertionSortTest() {}
-void SelectionSortTest() {}
+void SelectionSortTest() {
+  using namespace std;
+
+  vector<int> control_array;
+  control_array.push_back(2);
+  control_array.push_back(4);
+  control_array.push_back(53);
+  control_array.push_back(65);
+  control_array.push_back(165);
+  control_array.push_back(238);
+  control_array.push_back(432);
+  control_array.push_back(876);
+
+  vector<int> array;
+  array.push_back(4);
+  array.push_back(432);
+  array.push_back(53);
+  array.push_back(238);
+  array.push_back(2);
+  array.push_back(65);
+  array.push_back(165);
+  array.push_back(876);
+
+  SelectionSort(array);
+  assertm(control_array == array, "SelectionSortTest [failed]");
+  cout << "SelectionSortTest [passed]" << endl;
+
+}
 void HeapSortTest() {}
 void MergeSortTest() {}
 void QuickSortTest() {}
